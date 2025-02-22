3b3717d34a2d77221209e759dfaf575b111757fc
ibrahimeltbakh <eltabakh987@gmail.com>
2025-02-22 22:59:06 +0200

    update footer visability in register and login page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,9 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import masterCard from "../../assets/download.png";
 import amazonPay from "../../assets/amazon.png";
 import payPal from "../../assets/paypal.png";
+import { useSelector } from "react-redux";
 const Footer = () => {
+  const user = useSelector((state) => state.userToken);
   const social = [
     {
       icon: <LinkedInIcon />,
@@ -31,79 +33,83 @@ const Footer = () => {
   ];
   const parteners = [masterCard, amazonPay, payPal];
   return (
-    <Container
-      maxWidth="xl"
-      sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        flexDirection: { xs: "column", md: "row" },
-        gap: 3,
-        alignItems: "center",
-        background: "#fff",
-        padding: "30px  ",
-        marginTop: "40px",
-        borderTop: "2px solid teal",
-      }}>
-      <Stack
-        sx={{
-          height: "80px",
-          alignItems: "center",
-          justifyContent: "space-between",
-          padding: "5px 0",
-        }}>
-        <Typography variant="body1">Payment Partners</Typography>
-        <Stack flexDirection={"row"} gap={2}>
-          {parteners.map((part, index) => {
-            return (
-              <img
-                style={{ width: "60px", height: "60px" }}
-                key={index}
-                src={part}
-                alt="part"
-              />
-            );
-          })}
-        </Stack>
-      </Stack>
-      <Stack
-        sx={{
-          height: "80px",
-          alignItems: "center",
-          justifyContent: "space-between",
-          padding: "5px 0",
-        }}>
-        <Typography variant="body1">Follow Us</Typography>
-        <Stack flexDirection={"row"} gap={2}>
-          {social.map((page, index) => (
-            <a
-              key={index}
-              href={page.link}
-              target="_blank"
-              style={{ color: `${page.color}` }}>
-              {page.icon}
-            </a>
-          ))}
-        </Stack>
-      </Stack>
-      <Stack
-        sx={{
-          height: "80px",
-          alignItems: "center",
-          justifyContent: "space-between",
-          padding: "5px 0",
-        }}>
-        <Typography variant="body2">© 2025 All Rights Reserved.</Typography>
-        <Typography variant="body2">
-          Developed by:
-          <a
-            href="https://wa.me/+201024556364"
-            target="_blank"
-            style={{ color: "teal" }}>
-            ibrahim eltbakh
-          </a>
-        </Typography>
-      </Stack>
-    </Container>
+    <>
+      {user !== null && (
+        <Container
+          maxWidth="xl"
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: { xs: "column", md: "row" },
+            gap: 3,
+            alignItems: "center",
+            background: "#fff",
+            padding: "30px  ",
+            marginTop: "40px",
+            borderTop: "2px solid teal",
+          }}>
+          <Stack
+            sx={{
+              height: "80px",
+              alignItems: "center",
+              justifyContent: "space-between",
+              padding: "5px 0",
+            }}>
+            <Typography variant="body1">Payment Partners</Typography>
+            <Stack flexDirection={"row"} gap={2}>
+              {parteners.map((part, index) => {
+                return (
+                  <img
+                    style={{ width: "60px", height: "60px" }}
+                    key={index}
+                    src={part}
+                    alt="part"
+                  />
+                );
+              })}
+            </Stack>
+          </Stack>
+          <Stack
+            sx={{
+              height: "80px",
+              alignItems: "center",
+              justifyContent: "space-between",
+              padding: "5px 0",
+            }}>
+            <Typography variant="body1">Follow Us</Typography>
+            <Stack flexDirection={"row"} gap={2}>
+              {social.map((page, index) => (
+                <a
+                  key={index}
+                  href={page.link}
+                  target="_blank"
+                  style={{ color: `${page.color}` }}>
+                  {page.icon}
+                </a>
+              ))}
+            </Stack>
+          </Stack>
+          <Stack
+            sx={{
+              height: "80px",
+              alignItems: "center",
+              justifyContent: "space-between",
+              padding: "5px 0",
+            }}>
+            <Typography variant="body2">© 2025 All Rights Reserved.</Typography>
+            <Typography variant="body2">
+              Developed by:
+              <a
+                href="https://wa.me/+201024556364"
+                target="_blank"
+                style={{ color: "teal" }}>
+                ibrahim eltbakh
+              </a>
+            </Typography>
+          </Stack>
+        </Container>
+      )}
+    </>
   );
 };
 
